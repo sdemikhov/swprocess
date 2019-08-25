dde19678b84edf9238b54a766bac9bda88015c84
sdemikhov <demikhov.sergey@gmail.com>
2019-08-25 20:52:08 +0400

    update massive_send methods, your can use different commands for each device

--- concurrent_run.py
+++ concurrent_run.py
@@ -4,42 +4,41 @@ from swprocess import logger
 from swprocess.telnet_connect import connect
 
 
-def massive_send_commands(devices, commands, limit):
+def massive_send_commands(devices, limit):
     return _massive_send(
         _generate_connections(devices),
-        commands,
         'send_commands',
         limit
         )
 
    
-def massive_send_config_commands(devices, commands, limit):
-    connections = _generate_connections(devices)
+def massive_send_config_commands(devices, limit):
     return _massive_send(
         _generate_connections(devices),
-        commands,
         'send_config_commands',
         limit
         )
 
+
 def _generate_connections(devices):
-    for device in devices:
-        yield connect(**device)
+    for device, commands in devices:
+        if isinstance(commands, str):
+            commands = [commands]
+        yield (connect(**device), commands)
 
 
-def _massive_send(connections, commands, commands_type, limit):
+def _massive_send(connections, commands_type, limit):
     data = []
-    if isinstance(commands, str):
-        commands = [commands]
     with ThreadPoolExecutor(max_workers=limit) as executor:
         futures = [executor.submit(_run_commands,
                                    connect,
                                    commands,
                                    commands_type)
-                    for connect in connections]
+                    for connect, commands in connections]
         for f in as_completed(futures):
             data.append(f.result())
         return data
+
 
 def _run_commands(connect, commands, commands_type):
     ip = connect.ip
